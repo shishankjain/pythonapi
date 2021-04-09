54062339d53be5aeba862c9c39baf96d2ee2cc29
Shishank Jain <shishank.jain_cs18@gla.ac.in>
2021-04-09 21:49:32 +0530

    made put and get function (tested and working)

--- run.py
+++ run.py
@@ -2,5 +2,9 @@ import requests
 
 url="http://127.0.0.1:5000/"
 
-response =requests.get(url + "helloworld")
+response =requests.put(url + "video/1", {"likes":10, "name":"Shishank", "views": 100000})
+print (response.json())
+input()
+
+response =requests.get(url + "video/1")
 print (response.json())
